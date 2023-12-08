9f7b5bb144fb0c483b4a593309ae396f616ba9bd
Sergeev Viktor <equalizersva@yandex.ru>
2023-12-08 14:57:30 +0300

    feat: add form login & logout

--- src/main/java/ru/sva/taskmanagementsystem/controller/Controller.java
+++ src/main/java/ru/sva/taskmanagementsystem/controller/Controller.java
@@ -1,6 +1,7 @@
 package ru.sva.taskmanagementsystem.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.orm.hibernate5.SpringSessionContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,11 @@ import java.security.Principal;
 @RestController
 @RequiredArgsConstructor
 public class Controller {
+    @GetMapping("/")
+    public String homePage() {
+        return "Welcome to the home page!";
+    }
+
     @GetMapping("/unsecured")
     public String unsecuredData() {
         return "Unsecured data";
